ecad800ea117bde9b316a03c7e9d882654bf11aa
simplerogic <lajohnson12@gmail.com>
2016-01-13 15:49:01 -0700

    I am changing example two to demonstrate recommiting

--- example2.java
+++ example2.java
@@ -1,9 +1,9 @@
 public class Example{
 	public static void main(String[] args){
 
-	System.out.println("Test")
+	System.out.println("Test");
 
-
+	System.out.println("This i snow different");
 
 	}
 }
